d1076011f3800cc33540fa6343f99086e92e38b3
harish <harishbabukvmba@gmail.com>
2023-09-05 13:02:50 +0530

    commit message

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Harish Babu\OneDrive\Desktop\Django\librarymanagement-master\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
